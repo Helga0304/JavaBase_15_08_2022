6f15e37852e6c9333e2b5eaa449ee32985747cf1
Lianova Olga <lyanovaolya@gmail.com>
2022-08-24 21:57:53 +0300

    lesson 3

--- src/main/java/com/hillel/lianova/lessons/homework3/Main.java
+++ src/main/java/com/hillel/lianova/lessons/homework3/Main.java
@@ -1,5 +1,24 @@
 package com.hillel.lianova.lessons.homework3;
 
 public class Main {
+    public static void main(String[] args) {
 
+        int a = 9;
+        int b = 6;
+        int c = 3;
+
+        int resultVolume = (a * b * c );
+
+        System.out.println(resultVolume);
+        System.out.println();
+
+        String str = ("parallelipipid volume = " + a * b * c );
+
+        System.out.println(str);
+
+        int sum = a + b + c;
+
+        System.out.println();
+        System.out.println("length = " + sum );
+    }
 }
